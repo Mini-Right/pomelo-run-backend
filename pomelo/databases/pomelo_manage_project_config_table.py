4ef66a28b8f7fea1c87fd38e19733f3b01ae1919
Mini-Right <www@anyu.wang>
2023-02-27 10:43:41 +0800

    fix: 修复项目负责人唯一

--- pomelo/databases/pomelo_manage_project_config_table.py
+++ pomelo/databases/pomelo_manage_project_config_table.py
@@ -17,4 +17,4 @@ class PomeloManageProjectConfigTable(Base):
     __table_args__ = ({'comment': '项目配置表'})
     project_id = Column(VARCHAR(50), default=gen_id, primary_key=True, index=True)
     project_name = Column(VARCHAR(50), nullable=False, unique=True, comment="项目名称")
-    owner_user = Column(VARCHAR(50), nullable=False, unique=True, comment="项目负责人ID")
+    owner_user = Column(VARCHAR(50), nullable=False, comment="项目负责人ID")
